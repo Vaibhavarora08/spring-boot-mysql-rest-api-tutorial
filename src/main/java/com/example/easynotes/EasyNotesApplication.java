efe2f90d4a2ceb201b60bca8ed54793e7b6c3cb7
Vaibhavarora08 <38854840+Vaibhavarora08@users.noreply.github.com>
2018-11-29 22:19:43 +0530

    Update EasyNotesApplication.java

--- src/main/java/com/example/easynotes/EasyNotesApplication.java
+++ src/main/java/com/example/easynotes/EasyNotesApplication.java
@@ -2,10 +2,10 @@ package com.example.easynotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class EasyNotesApplication {
 
 	public static void main(String[] args) {
